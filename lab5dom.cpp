7281c4161d52c0afcaee6d574b66522fbd12a639
klbmarysia <Mkalabinska76@gmail.com>
2025-11-07 23:35:37 +0100

    Dodaj pliki projektów.

---
+++ lab5dom.cpp
@@ -0,0 +1,28 @@
+﻿// lab5dom.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+#include <cstdlib>
+int main() {
+	int tab[10];
+	double srednia = 0;
+
+	for (int i = 0; i < 10; i++) {
+		tab[i] = rand() % 10;
+	}
+	for (int k = 0; k < 10; k++) {
+		cout << tab[k] << endl;
+		srednia = tab[k] + srednia;
+	}
+	srednia = srednia / 10;
+	cout << srednia << endl;
+	int a = 0;
+	for (int n = 0; n < 10; n++) {
+		if (tab[n] < srednia) {
+			a++;
+		}
+	}
+	cout << "jest " << a << "liczb mniejszych od sredniej" << endl;
+}
